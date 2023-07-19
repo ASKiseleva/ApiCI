be8ae8a0c9f3d0313e87bb91752666259ea59b55
Neto test <anna.shuliko@bk.ru>
2023-07-19 18:25:04 +0300

    Test4Failed

--- src/test/java/ru/netology/rest/MobileBankApiTestV2.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV2.java
@@ -23,4 +23,20 @@ class MobileBankApiTestV2 {
           .contentType(ContentType.JSON)
       ;
     }
+
+    static class MobileBankApiTestV1 {
+        @Test
+        void shouldReturnDemoAccounts() {
+          // Given - When - Then
+          // Предусловия
+          given()
+              .baseUri("http://localhost:9999/api/v1")
+          // Выполняемые действия
+          .when()
+              .get("/demo/accounts")
+          // Проверки
+          .then()
+              .statusCode(200);
+        }
+    }
 }
